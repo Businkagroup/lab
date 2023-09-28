727f1c2f48209c95ae2f7aeee7d6e476e7f4763c
Businkagroup <146329662+Businkagroup@users.noreply.github.com>
2023-09-28 17:41:52 +0200

    Improved readability

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 int main(){
+    
     int i;
+    
     i = 69;
+    
     int number = 72;
+    
     int i = 69 + 72;
+    
     int array [] = {0, 0, 0};
+    
     int *iPointer = &i;
+    
     number = *iPointer;
+    
     *iPointer = 15;
+    
     iPointer = &array[0];
+    
     iPointer = iPointer + 2;
+    
     *iPointer = 6;
+    
     array[0] = 5;
  
   return 0;
